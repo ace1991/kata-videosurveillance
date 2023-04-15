4075aeb224c5bb4de4b16b9ba20a3520ba572ecd
acaleza <adrian.calvo1@gmail.com>
2023-04-15 18:35:56 +0200

    Añadidos test para los 2 primeros requisitos

---
+++ src/core/surveillanceController.ts
@@ -0,0 +1,19 @@
+export interface MotionSensor {
+    isDetectingMotion(): boolean;
+}
+
+export interface VideoRecorder {
+    startRecording(): void;
+    stopRecording(): void;
+}
+export class SurveillanceController {
+    constructor(private sensor: MotionSensor, private recorder: VideoRecorder) {}
+
+    recordMotion() {
+        if(this.sensor.isDetectingMotion()){
+            this.recorder.startRecording();
+        }else {
+            this.recorder.stopRecording()
+        }
+    }
+}
